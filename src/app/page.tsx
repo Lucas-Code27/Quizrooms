a3ae8bf731bd8aa62f98f0e8503f151b968fdda3
Lucas-Code <lucasghenneman@gmail.com>
2024-04-23 17:53:39 -0700

    Fixed code syntax error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,11 @@
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
-        <h1 class="Header">Welcome to the Quizrooms</h1>
-        <h6 class="SubHeader">The next level of Quizzes</h6>
+        <h1 className="Header">Welcome to the Quizrooms</h1>
+        <h6 className="SubHeader">The next level of Quizzes</h6>
       </div>
-      <div class="Content">
-      <h2 class="building">This page is under contruction. Come back later</h2>
+      <div className="Content">
+      <h2 className="building">This page is under contruction. Come back later</h2>
       </div>
     </main>
   );
