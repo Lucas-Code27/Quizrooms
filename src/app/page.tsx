e224362249881a78e3bf0f5474c823fd0677604c
Lucas-Code <lucasghenneman@gmail.com>
2024-04-24 14:28:04 -0700

    Created Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,25 @@
       <div>
         <h1 className="Header">Welcome to the Quizrooms</h1>
         <h6 className="SubHeader">The next level of Quizzes</h6>
-        <div className="Content">
-          <h2 className="building">This page is under contruction. Come back later</h2>
+        <div>
+          <h2 className="Intro">Who are you?</h2>
         </div>
+      </div>
+
+      <div className="Options">
+        <button className="Button">
+          <h1 className="bText">Player</h1>
+          <div>
+            <p className="bTexts">Joining quizzes</p>
+          </div>
+        </button>
+
+        <button className="Button">
+          <h1 className="bText">Host</h1>
+          <div>
+            <p className="bTexts">Hosting and creating quizzes for others to join</p>
+          </div>
+        </button>
       </div>
     </main>
   );
