cef929fa78f733892413155495c26ab60e205438
Lucas-Code <lucasghenneman@gmail.com>
2024-04-24 14:01:54 -0700

    Speed insights

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,9 @@
       <div>
         <h1 className="Header">Welcome to the Quizrooms</h1>
         <h6 className="SubHeader">The next level of Quizzes</h6>
-      </div>
-      <div className="Content">
-      <h2 className="building">This page is under contruction. Come back later</h2>
+        <div className="Content">
+          <h2 className="building">This page is under contruction. Come back later</h2>
+        </div>
       </div>
     </main>
   );
