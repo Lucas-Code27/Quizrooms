186bac899ecea3e31916cad0136cff2ba65712df
Lucas-Code <lucasghenneman@gmail.com>
2024-04-23 17:17:36 -0700

    Landing Page
    Made basic page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
-import Image from "next/image";
-
-export default function Home() {
+ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>
+        <h1>Welcome to the Quizrooms</h1>
+      </div>
       
     </main>
   );
