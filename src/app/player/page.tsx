a4a4c737f8257002bee7902723d042cb488726c9
Lucas-Code <lucasghenneman@gmail.com>
2024-04-30 19:51:05 -0700

    Added room code input

--- src/app/player/page.tsx
+++ src/app/player/page.tsx
@@ -1,3 +1,5 @@
+import { TextInput, Container } from "@mantine/core";
+
  export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -8,6 +10,19 @@
           <h2 className="Intro">You are a player</h2>
         </div>
       </div>
+      <Container bg="#e0e0e0"
+       styles={{
+        root: {
+          margin: '20px',
+          padding: '20px'
+        },
+      }}>      
+        <TextInput
+      size="lg"
+      label="Enter Room Code"
+      placeholder="Enter Code Here"
+      />
+      </Container>
     </main>
   );
 }
