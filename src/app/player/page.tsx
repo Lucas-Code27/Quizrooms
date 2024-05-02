a8b7f146141e02158643242ab6ae5213e0bd27bf
Lucas-Code <lucasghenneman@gmail.com>
2024-05-01 17:18:19 -0700

    Updated Player Screen

--- src/app/player/page.tsx
+++ src/app/player/page.tsx
@@ -1,27 +1,24 @@
-import { TextInput, Container } from "@mantine/core";
+import { TextInput, Container, Center } from "@mantine/core";
+import Link from "next/link";
 
  export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
-        <h1 className="Header">Welcome to the Quizrooms</h1>
-        <h6 className="SubHeader">The next level of Quizzes</h6>
-        <div>
-          <h2 className="Intro">You are a player</h2>
-        </div>
-      </div>
-      <Container bg="#e0e0e0"
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">  
+      <Link href="/">
+        <Container bg="#e0e0e0">Return To Home Page</Container>
+      </Link>
+      <Container bg="#e0e0e0" className = "CodeInput"
        styles={{
         root: {
           margin: '20px',
           padding: '20px'
         },
-      }}>      
+      }}>
         <TextInput
-      size="lg"
-      label="Enter Room Code"
-      placeholder="Enter Code Here"
-      />
+          size="lg"
+          label="Enter Room Code"
+           placeholder="Enter Code Here"
+        />
       </Container>
     </main>
   );
