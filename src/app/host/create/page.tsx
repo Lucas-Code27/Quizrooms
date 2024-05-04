f951ac429c8545efe1485c2f57972091d3f5719d
Lucas-Code <lucasghenneman@gmail.com>
2024-05-04 10:08:01 -0700

    funny cat

---
+++ src/app/host/create/page.tsx
@@ -0,0 +1,20 @@
+import { Button, Container, FileInput } from "@mantine/core";
+import Link from "next/link";
+
+ export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>
+      <Link href="/host">
+        <Container bg="#e0e0e0" className="Return">Return To Host Screen</Container>
+      </Link>
+        <div>
+          <h2 className="Intro">Quiz Creator</h2>
+        </div>
+        <a 
+        download href="/funny cat.jpg"
+        >cat</a>
+      </div>
+    </main>
+  );
+}
