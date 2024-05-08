533b32d23f7a0cf252b8a2c7a58173bcd6196f49
Lucas-Code <lucasghenneman@gmail.com>
2024-05-08 13:42:55 -0700

    small change on header work

--- src/app/host/create/page.tsx
+++ src/app/host/create/page.tsx
@@ -6,14 +6,18 @@ import Link from "next/link";
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
         <div>
-          <h2 className="Intro">Quiz Creator</h2>
-          <a 
-          download href="/funny cat.jpg"
-          >
-            <div className="download">
-            <p>Download Quiz</p>
+            <div>
+              <Container bg="blue" className="cheader">
+              <h2 className="Intro">Quiz Creator</h2>
+              <a 
+              download href="/funny cat.jpg"
+              >
+                <div className="download">
+                <p>Download Quiz</p>
+                </div>
+              </a>
+              </Container>
             </div>
-          </a>
           </div>
       <Link href="/host">
         <Container bg="#e0e0e0" className="Return">Return To Host Screen</Container>
