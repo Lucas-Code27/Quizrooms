eae4aa3c4d8f68dd666a0aeb961a8df6a9a92537
Lucas-Code <lucasghenneman@gmail.com>
2024-06-09 07:10:32 -0700

    Fresh Wipe

--- src/app/example/page.tsx
+++
@@ -1,8 +0,0 @@
- import { NavbarMinimal } from "@/component/test/NavbarMinimal";
- export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <NavbarMinimal/>
-    </main>
-  );
-}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,46 +1,10 @@
-import { Button, Container } from "@mantine/core";
-import Link from "next/link";
 
  export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
-        <h1 className="Header">Welcome to the Quizrooms</h1>
-        <h6 className="SubHeader">The next level of Quizzes</h6>
-        <div>
-          <h2 className="Intro">What are you doing today?</h2>
-        </div>
+      <div className="TitleBox">
+        <h1>Welcome to the Quizrooms</h1>
       </div>
-
-      <div className="Options">
-          <Link href="/player">
-            <Container px={30} bg="#e0e0e0" size="sm"
-              styles={{
-                root: {
-                  margin: '20px', // Adjust this value as needed
-                },
-              }}>
-            <h1 className="bHead">Player</h1>
-              <div>
-              < p className="bText">Play against others in a Quiz</p>
-              </div>
-            </Container>
-         </Link>
-          <Link href="/host">
-            <Container px={30} bg="#e0e0e0" size="sm"
-              styles={{
-                root: {
-                  
-                  margin: '20px', // Adjust this value as needed
-                },
-              }}>
-            <h1 className="bHead">Host</h1>
-              <div>
-                <p className="bText">Hosting and creating quizzes for others to join</p>
-              </div>
-            </Container>
-          </Link>
-        </div>
     </main>
   );
 }
